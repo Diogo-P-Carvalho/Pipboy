d52413ad608196a5a93c622d152b7caf519c9e98
Diogo Carvalho <34646795+Diogo-P-Carvalho@users.noreply.github.com>
2018-08-13 21:45:59 +0100

    Item list

---
+++ JS/pipboy.app.js
@@ -0,0 +1,33 @@
+/**Created by Diogo on 2018-08-13 */
+
+$(document).ready(function(){
+    var weapons = [
+    {
+        "name": "44_pistol",
+        "damage": 48,
+        "fire_rate": 6,
+        "range": 119,
+        "accuracy": 66,
+        "weight": 4.2,
+        "value": 99
+    },
+    {
+        "name": "10mm_pistol",
+        "damage": 18,
+        "fire_rate": 46,
+        "range": 119,
+        "accuracy": 61,
+        "weight": 4.2,
+        "value": 53
+    },
+    {
+        "name": "assault_rifle",
+        "damage": 30,
+        "fire_rate": 40,
+        "range": 119,
+        "accuracy": 72,
+        "weight": 13.1,
+        "value": 144
+    }
+    ]
+});
