a6bbf567925bc6e302114c67eba796d31a2ca4cb
Diogo Carvalho <34646795+Diogo-P-Carvalho@users.noreply.github.com>
2018-08-15 19:00:50 +0100

    Touches

--- JS/pipboy.app.js
+++ JS/pipboy.app.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     {
         "name": "44_pistol",
         "damage": 48,
+        "ammo": ".44",
         "fire_rate": 6,
         "range": 119,
         "accuracy": 66,
@@ -14,6 +15,7 @@ $(document).ready(function(){
     {
         "name": "10mm_pistol",
         "damage": 18,
+        "ammo": "10mm",
         "fire_rate": 46,
         "range": 119,
         "accuracy": 61,
@@ -23,11 +25,35 @@ $(document).ready(function(){
     {
         "name": "assault_rifle",
         "damage": 30,
+        "ammo": "5.56mm",
         "fire_rate": 40,
         "range": 119,
         "accuracy": 72,
         "weight": 13.1,
         "value": 144
     }
-    ]
+    ];
+
+    $('.item-list a').on('click', function(e){
+        $('.item-list a').removeClass('active');
+        $(e.currentTarget).addClass('active');
+    });
+
+    $('.item-list a').on('mouseenter', function(e){
+        var current_item = $(e.currentTarget).attr('class');
+
+        for(item in weapons){
+            if(weapons[item].name == current_item){
+                var container = $('.item-stats');
+
+                container.find('.damage').html(weapons[item].damage);
+                container.find('.ammo').html(weapons[item].ammo);
+                container.find('.fire_rate').html(weapons[item].fire_rate);
+                container.find('.range').html(weapons[item].range);
+                container.find('.accuracy').html(weapons[item].accuracy);
+                container.find('.weight').html(weapons[item].weight);
+                container.find('.value').html(weapons[item].value);
+            }
+        }
+    });
 });
